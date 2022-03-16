d539fa31bce971205d7f3c10184ceb9d5cb3102a
Leon Woestenberg <leon@sidebranch.com>
2022-03-16 02:05:28 +0100

    Add leading zeroes counter from Tom Verbeure.
    Signed-off-by: Leon Woestenberg <leon@sidebranch.com>

---
+++ src/main/scala/corundum/LeadingZeroes.scala
@@ -0,0 +1,29 @@
+package corundum
+
+import spinal.core._
+import spinal.lib._
+
+object LeadingZeroes {
+
+    // Calculate leading zeros. Solution is based on method described here:
+    // https://electronics.stackexchange.com/questions/196914/verilog-synthesize-high-speed-leading-zero-count
+    // Code by @typingArtist on SpinalHDL gitter channel: https://gitter.im/SpinalHDL/SpinalHDL?at=5bbe075e435c2a518e81dd83
+
+    def apply(input: Bits): UInt = calcOnes(~input).resize(log2Up(input.getWidth+1))
+
+    def calcOnes(input: Bits): UInt = input.getWidth match {
+        case 0 => U""
+        case 1 => input.asUInt
+        case a => {
+            val leftBits = 1 << (log2Up(a)-1)
+            val upper = calcOnes(input.resizeLeft(leftBits))
+            val lower = calcOnes(input.resize(a - leftBits)).resize(upper.getWidth)
+            (upper.msb ## lower.msb).mux(
+                B"11"   -> U"10" @@ upper.resize(upper.getWidth-1),
+                B"10"   -> U"01" @@ lower.resize(lower.getWidth-1),
+                default -> U"00" @@ upper.resize(upper.getWidth-1)
+                )
+        }
+    }
+
+}
