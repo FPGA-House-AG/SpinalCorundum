29b174aa546098615d3da760d812fc4a892ef575
Leon Woestenberg <leon@sidebranch.com>
2022-11-08 22:12:45 +0100

    Add renameAxiIO() to CorundumFrame.scala

--- src/main/scala/corundum/CorundumFrame.scala
+++ src/main/scala/corundum/CorundumFrame.scala
@@ -18,6 +18,22 @@ object CorundumFrame {
     val frame = CorundumFrame(dataWidth = 512)
     frame
   }
+
+    // Rename SpinalHDL library defaults to AXI naming convention
+  def renameAxiIO(io: Bundle): Unit = {
+    io.flatten.foreach(bt => {
+      if(bt.getName().contains("_payload_fragment")) bt.setName(bt.getName().replace("_payload_fragment", "_tdata"))
+      if(bt.getName().contains("_payload_last")) bt.setName(bt.getName().replace("_payload_last", "_tlast"))
+      if(bt.getName().contains("_payload"))  bt.setName(bt.getName().replace("_payload",  ""))
+      if(bt.getName().contains("_fragment")) bt.setName(bt.getName().replace("_fragment", ""))
+      if(bt.getName().contains("_valid"))    bt.setName(bt.getName().replace("_valid",    "_tvalid"))
+      if(bt.getName().contains("_ready"))    bt.setName(bt.getName().replace("_ready",    "_tready"))
+      if(bt.getName().contains("_last"))     bt.setName(bt.getName().replace("_last",     "_tlast"))
+      if(bt.getName().contains("_tdata_"))     bt.setName(bt.getName().replace("_tdata_",     "_"))
+      if(bt.getName().contains("reset"))     bt.setName(bt.getName().replace("reset",     "rst"))
+      if(bt.getName().startsWith("io_")) bt.setName(bt.getName().replaceFirst("io_",""))
+    })
+  }
 }
 
 class CorundumFrame(dataWidth : Int, userWidth : Int) extends Bundle {
