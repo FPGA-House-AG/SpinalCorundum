febe5c8fcb4b5bfcd11b0b85c235f3380ce3d734
Leon Woestenberg <leon@sidebranch.com>
2022-11-13 02:19:01 +0100

    Match Wireguard key lookup with LookupTable latency.
    Signed-off-by: Leon Woestenberg <leon@sidebranch.com>

--- src/main/scala/corundum/AxisWireguardKeyLookup.scala
+++ src/main/scala/corundum/AxisWireguardKeyLookup.scala
@@ -34,26 +34,57 @@ case class AxisWireguardKeyLookup(dataWidth : Int) extends Component {
     val receiver = out UInt(32 bits)
     val counter = out UInt(32 bits)
     // key will have 2 cycles latency relative to receiver
-    val key = in Bits(128 bits)
+    val key = out Bits(16 bits)
   }
 
+  // register Wireguard Type 4 header fields
   val receiver = RegNextWhen(io.sink.payload(4 * 8, 32 bits), io.sink.isFirst)
   val counter = RegNextWhen(io.sink.payload(8 * 8, 32 bits), io.sink.isFirst)
+
   io.receiver := receiver.asUInt
   io.counter := counter.asUInt
 
   val sink_is_first = io.sink.isFirst
 
-  //io.source <-< io.sink
-  // strip off first word - @TODO does this work?
-  // AND what if packet is zero size, i.e. nothing behind header?
-  io.source.valid := RegNextWhen(io.sink.valid & !io.sink.isFirst, io.sink.ready) init(False)
-  io.source.payload := RegNextWhen(io.sink.payload, io.sink.ready)
-  io.source.last := RegNextWhen(io.sink.last, io.sink.ready)
-  io.sink.ready := io.source.ready
+  val y = Stream(Fragment(Bits(dataWidth bits)))
 
-  // @TODO round up to next 16 bytes
-  io.source_length := RegNext(io.sink_length - 128/8)
+  y.valid := RegNextWhen(io.sink.valid & !io.sink.isFirst, io.sink.ready) init(False)
+  y.payload := RegNextWhen(io.sink.payload, io.sink.ready)
+  y.last := RegNextWhen(io.sink.last, io.sink.ready)
+  io.sink.ready := y.ready
+
+  io.source <-< y
+
+  // remove 128 bits Wireguard Type 4 header from length
+  val unpadded_length_out = RegNextWhen(io.sink_length - 128/8, io.sink.isFirst)
+  val padded16_length_out = RegNext(((unpadded_length_out + 15) >> 4) << 4)
+  //val padded16_length_out = RegNext(unpadded_length_out)
+  io.source_length := RegNext(padded16_length_out)
+
+  val has_internal_test_lut = true
+
+  if (has_internal_test_lut) {
+    val keys_num = 256
+    val lut = LookupTable(16, keys_num)
+    lut.mem.initBigInt(Seq.range(0xA000, 0xA000 + keys_num))
+    lut.io.portA.en := True
+    lut.io.portA.wr := False
+    lut.io.portA.wrData := 0
+    lut.io.portA.addr := receiver.resize(log2Up(keys_num)).asUInt
+
+    io.key := lut.io.portA.rdData
+
+    lut.io.portB.clk := ClockDomain.current.readClockWire
+    lut.io.portB.rst := False
+    lut.io.portB.en := True
+    lut.io.portB.wr := False
+    lut.io.portB.wrData := 0
+    lut.io.portB.addr := 0
+  } else {
+    io.key := 0
+  }
+  val source_is_first = io.source.isFirst
+
 
   // Rename SpinalHDL library defaults to AXI naming convention
   private def renameIO(): Unit = {
