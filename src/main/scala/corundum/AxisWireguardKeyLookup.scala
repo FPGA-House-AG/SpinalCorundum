781f605f7a1b12c22e9bce6e9a1108d92c48f97a
Leon Woestenberg <leon@sidebranch.com>
2022-11-15 02:15:26 +0100

    Modify AxisWireguardType4 for ChaCha20-Poly1305 interface.
    Signed-off-by: Leon Woestenberg <leon@sidebranch.com>

--- src/main/scala/corundum/AxisWireguardKeyLookup.scala
+++ src/main/scala/corundum/AxisWireguardKeyLookup.scala
@@ -22,7 +22,7 @@ object AxisWireguardKeyLookup {
  * source_length is the output packet (Ethernet payload)
  */
 
-case class AxisWireguardKeyLookup(dataWidth : Int) extends Component {
+case class AxisWireguardKeyLookup(dataWidth : Int, has_internal_test_lut : Boolean = false) extends Component {
   require(dataWidth == 128, "Assuming Wireguard Type 4 header is one word")
   val io = new Bundle {
     // I/O is only the Corundum Frame tdata payload
@@ -33,52 +33,72 @@ case class AxisWireguardKeyLookup(dataWidth : Int) extends Component {
     val source_length = out UInt(12 bits)
     // outgoing lookup index for the key
     val receiver = out UInt(32 bits)
-    val counter = out UInt(32 bits)
+    val counter = out UInt(64 bits)
     // key will have 2 cycles latency relative to receiver
-    val key = out Bits(16 bits)
+    val key_in = (!has_internal_test_lut) generate (in Bits(256 bits))
+    val key_out = out Bits(256 bits)
   }
 
   // register Wireguard Type 4 header fields
   val receiver = RegNextWhen(io.sink.payload(4 * 8, 32 bits), io.sink.isFirst)
-  val counter = RegNextWhen(io.sink.payload(8 * 8, 32 bits), io.sink.isFirst)
+  val counter = RegNextWhen (io.sink.payload(8 * 8, 64 bits), io.sink.isFirst)
 
+  // drive external key LUT, expect key_in valid after two clock cycles
   io.receiver := receiver.asUInt
   io.counter := counter.asUInt
 
   val sink_is_first = io.sink.isFirst
 
-  val y = Stream(Fragment(Bits(dataWidth bits)))
-
-  // remove 128 bits Wireguard Type 4 header on output by clearing valid
-  // register to achieve one pipeline stage
-  y.valid := RegNextWhen(io.sink.valid & !io.sink.isFirst, io.sink.ready) init(False)
-  y.payload := RegNextWhen(io.sink.payload, io.sink.ready)
-  y.last := RegNextWhen(io.sink.last, io.sink.ready)
-  io.sink.ready := y.ready
-
-  // add one extra pipeline stage
-  io.source <-< y
-
-  // remove 128 bits Wireguard Type 4 header from output length
-  val unpadded_length_out = RegNextWhen(io.sink_length - 128/8, io.sink.isFirst)
+  // remove 128 bits Wireguard Type 4 header and 128 bits tag from output length
+  val unpadded_length_out = RegNextWhen(io.sink_length - 128/8 - 128/8, io.sink.isFirst)
   // round up to next 16 bytes (should we always do this? -- Ethernet MTU?)
   val padded16_length_out = RegNext(((unpadded_length_out + 15) >> 4) << 4)
   // during testing, do not pad, to verify correct propagation delay in simulator
   //val padded16_length_out = RegNext(unpadded_length_out)
+
+  val x = Stream(Fragment(Bits(dataWidth bits)))
+
+  // two cycles latency
+  x <-< io.sink.stage()
+
+  val y = Stream(Fragment(Bits(dataWidth bits)))
+  // pipeline x into y, thereby replacing first beat
+    // modify 128 bits Wireguard Type 4 header on output by clearing valid
+    // register to achieve one pipeline stage
+    // replace reserved field with payload length
+    val header_payload = x.payload(127 downto 32) ## padded16_length_out.resize(24).asBits ## x.payload(7 downto 0)
+    val y_payload = Bits(dataWidth bits)
+    y_payload.assignFromBits(Mux(x.isFirst, header_payload, x.payload))
+
+    val delete_header = x.isFirst & False
+
+    y.valid := RegNextWhen(x.valid /*& !delete_header*/, x.ready) init(False)
+    y.payload := RegNextWhen(y_payload, x.ready)
+    y.last := RegNextWhen(x.last, x.ready)
+    x.ready := y.ready
+
+  // add two extra pipeline stages
+  io.source << y
+
   io.source_length := RegNext(padded16_length_out)
 
-  val has_internal_test_lut = true
-
-  if (has_internal_test_lut) {
+  val pass_key = (!has_internal_test_lut) generate new Area{
+    // optional hardware here
+    io.key_out := io.key_in
+  }
+  val pass_internal_key = (has_internal_test_lut) generate new Area{
     val keys_num = 256
-    val lut = LookupTable(16, keys_num)
-    lut.mem.initBigInt(Seq.range(0xA000, 0xA000 + keys_num))
+    val lut = LookupTable(256/*bits*/, keys_num)
+    //                                            1122334455667788990011223344556677889900112233445566778899001122
+    lut.mem.initBigInt(Seq.fill(keys_num)(BigInt("808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f", 16)))
+
+    //lut.mem.initBigInt(Seq.range(0xA000, 0xA000 + keys_num))
     lut.io.portA.en := True
     lut.io.portA.wr := False
     lut.io.portA.wrData := 0
     lut.io.portA.addr := receiver.resize(log2Up(keys_num)).asUInt
 
-    io.key := lut.io.portA.rdData
+    io.key_out := lut.io.portA.rdData
 
     lut.io.portB.clk := ClockDomain.current.readClockWire
     lut.io.portB.rst := False
@@ -86,11 +106,8 @@ case class AxisWireguardKeyLookup(dataWidth : Int) extends Component {
     lut.io.portB.wr := False
     lut.io.portB.wrData := 0
     lut.io.portB.addr := 0
-  } else {
-    io.key := 0
   }
   val source_is_first = io.source.isFirst
-
 
   // Rename SpinalHDL library defaults to AXI naming convention
   private def renameIO(): Unit = {
@@ -115,13 +132,70 @@ case class AxisWireguardKeyLookup(dataWidth : Int) extends Component {
 //Generate the AxisWireguardKeyLookup's Verilog
 object AxisWireguardKeyLookupVerilog {
   def main(args: Array[String]) {
-    SpinalVerilog(new AxisWireguardKeyLookup(128))
+    SpinalVerilog(new AxisWireguardKeyLookup(128, has_internal_test_lut = true))
   }
 }
 
 //Generate the AxisWireguardKeyLookup's VHDL
 object AxisWireguardKeyLookupVhdl {
   def main(args: Array[String]) {
-    SpinalVhdl(new AxisWireguardKeyLookup(128))
+    SpinalVhdl(new AxisWireguardKeyLookup(128, has_internal_test_lut = true))
+  }
+}
+
+case class AxisWireguardType4() extends Component {
+ final val corundumDataWidth = 512
+ final val cryptoDataWidth = 128
+ final val maxPacketLength = 1534
+  // 1534 rounded up 2048/(512/8) == 32
+
+  val io = new Bundle {
+    // I/O is only the Corundum Frame tdata payload
+    val sink = slave Stream Fragment(CorundumFrame(corundumDataWidth))
+    val source = master Stream(Fragment(Bits(cryptoDataWidth bits)))
+    val key = out Bits(256 bits)
+  }
+  val stash = CorundumFrameStash(corundumDataWidth, 32)
+  stash.io.sink << io.sink
+
+  // extract only TDATA into fragment
+  val x = Stream(Fragment(Bits(corundumDataWidth bits)))
+  val fff = Fragment(Bits(corundumDataWidth bits))
+  fff.last := stash.io.source.payload.last
+  fff.fragment := stash.io.source.payload.fragment.tdata
+  x << stash.io.source.translateWith(fff)
+
+  val downsizer = AxisWidthAdapter(corundumDataWidth, cryptoDataWidth)
+  downsizer.io.sink << x
+  downsizer.io.sink_length := stash.io.length
+  val type4 = AxisWireguardKeyLookup(cryptoDataWidth, has_internal_test_lut = false)
+  type4.io.sink << downsizer.io.source
+  type4.io.sink_length := downsizer.io.source_length
+
+  io.source << type4.io.source
+  io.key := type4.io.key_out
+
+  val keys_num = 256
+  val lut = LookupTable(256/*bits*/, keys_num)
+  lut.mem.initBigInt(Seq.fill(keys_num)(BigInt("80 81 82 83 84 85 86 87 88 89 8a 8b 8c 8d 8e 8f 90 91 92 93 94 95 96 97 98 99 9a 9b 9c 9d 9e 9f".split(" ").reverse.mkString(""), 16)))
+
+  lut.io.portA.en := True
+  lut.io.portA.wr := False
+  lut.io.portA.wrData := 0
+  lut.io.portA.addr := type4.io.receiver.resize(log2Up(keys_num))
+  type4.io.key_in := lut.io.portA.rdData
+
+  lut.io.portB.clk := ClockDomain.current.readClockWire
+  lut.io.portB.rst := False
+  lut.io.portB.en := True
+  lut.io.portB.wr := False
+  lut.io.portB.wrData := 0
+  lut.io.portB.addr := 0
+
+}
+//Generate the AxisWireguardType4's VHDL
+object AxisWireguardType4Vhdl {
+  def main(args: Array[String]) {
+    SpinalVhdl(new AxisWireguardType4())
   }
 }
--- src/main/scala/corundum/AxisWireguardKeyLookupSim.scala
+++ src/main/scala/corundum/AxisWireguardKeyLookupSim.scala
@@ -14,7 +14,7 @@ object AxisWireguardKeyLookupSim {
     val keepWidth = dataWidth/8
     SimConfig
     .withFstWave
-    .doSim(new AxisWireguardKeyLookup(dataWidth)){dut =>
+    .doSim(new AxisWireguardKeyLookup(dataWidth, true)){dut =>
 
       SimTimeout(100000 * 10)
 
@@ -64,8 +64,10 @@ object AxisWireguardKeyLookupSim {
             dut.io.receiver.toBigInt, dut.io.counter.toBigInt)
           // during any valid beat, the receiver and counter should
           // be from the header bytes [8:4] and [12:9] resp.
-          if (dut.io.source.valid.toBoolean) {
+          if (dut.io.source.valid.toBoolean & first_beat) {
             assert((dut.io.receiver.toBigInt & 0x0F0F0F0F) == 0x08070605)
+          }
+          if (dut.io.source.valid.toBoolean) {
             assert((dut.io.counter.toBigInt & 0x0F0F0F0F) == 0x0C0B0A09)
           }
           in_packet_continuation = !dut.io.source.payload.last.toBoolean
@@ -168,6 +170,102 @@ object AxisWireguardKeyLookupSim {
           // Wait a rising edge on the clock
           dut.clockDomain.waitRisingEdge()
       }
+      dut.clockDomain.waitRisingEdge(8)
+    }
+  }
+}
+
+object AxisWireguardType4Sim {
+  def main(args: Array[String]) {
+    val dataWidth = 512
+    val maxDataValue = scala.math.pow(2, dataWidth).intValue - 1
+    val keepWidth = dataWidth/8
+    SimConfig
+    .withFstWave
+    .doSim(AxisWireguardType4()){dut =>
+
+      dut.io.sink.valid #= false
+
+      //Fork a process to generate the reset and the clock on the dut
+      dut.clockDomain.forkStimulus(period = 10)
+
+      var data0 = 0
+
+      var last0 = false
+      var valid0 = false
+      var tkeep0 = BigInt(0)
+      var pause = false
+
+      dut.clockDomain.waitSampling()
+
+      // 000  4c 61 64 69 65 73 20 61 6e 64 20 47 65 6e 74 6c  Ladies and Gentl
+      // 016  65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73  emen of the clas
+      // 032  73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63  s of '99: If I c
+      // 048  6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f  ould offer you o
+      // 064  6e 6c 79 20 6f 6e 65 20 74 69 70 20 66 6f 72 20  nly one tip for
+      // 080  74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73  the future, suns
+      // 096  63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69  creen would be i
+      // 112  74 2e                                            t.
+
+      // BigInt("4c616469657320616e642047656e746c656d656e206f662074686520636c617373206f66202739393a204966204920636f756c64206f6666657220796f75206f", 16) // Ladies and Gentlemen of the class of '99: If I could offer you o
+      // BigInt("6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73637265656e20776f756c642062652069742e0000000000000000000000000000", 16) // nly one tip for the future, sunscreen would be it.
+
+      val plaintext = Vector(
+        BigInt("04 00 00 00 11 22 33 44 c1 c2 c3 c4 c5 c6 c7 c8 4c 61 64 69 65 73 20 61 6e 64 20 47 65 6e 74 6c 65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73 73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63".split(" ").reverse.mkString(""), 16),
+        BigInt("6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f 6e 6c 79 20 6f 6e 65 20 74 69 70 20 66 6f 72 20 74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73 63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69".split(" ").reverse.mkString(""), 16),
+        BigInt("74 2e 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00".split(" ").reverse.mkString(""), 16)
+      )
+
+      //  BigInt("0400000011223344c1c2c3c4c5c6c7c84c616469657320616e642047656e746c656d656e206f662074686520636c617373206f66202739393a20496620492063", 16),
+      //  BigInt("6f756c64206f6666657220796f75206f6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73637265656e20776f756c642062652069", 16),
+      //  BigInt("742e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", 16)
+
+      var packet_length = 16 + 114 // bytes
+      var remaining = packet_length
+
+      var word_index = 0
+      // iterate over frame content
+      while (remaining > 0) {
+        printf("remaining = %d\n", remaining)
+        val tkeep_len = if (remaining >= keepWidth) keepWidth else remaining;
+        printf("tkeep_len = %d\n", tkeep_len)
+        valid0 = (Random.nextInt(8) > 2)
+        valid0 &= !pause
+        if (pause) pause ^= (Random.nextInt(16) >= 15)
+        if (!pause) pause ^= (Random.nextInt(128) >= 127)
+
+        assert(tkeep_len <= keepWidth)
+        tkeep0 = 0
+        data0 = 0
+        if (valid0) {
+          last0 = (remaining <= keepWidth)
+          for (i <- 0 until tkeep_len) {
+            tkeep0 = (tkeep0 << 1) | 1
+          }
+        }
+
+        dut.io.sink.valid #= valid0
+        dut.io.sink.payload.tdata #= plaintext(word_index)
+        dut.io.sink.last #= last0
+        dut.io.sink.payload.tkeep #= tkeep0
+
+        dut.io.source.ready #= (Random.nextInt(8) > 1)
+
+        // Wait a rising edge on the clock
+        dut.clockDomain.waitRisingEdge()
+
+        if (dut.io.sink.ready.toBoolean & dut.io.sink.valid.toBoolean) {
+          remaining -= tkeep_len
+          word_index += 1
+        }
+      }
+      dut.io.sink.valid #= false
+      dut.io.source.ready #= true
+      while (dut.io.source.valid.toBoolean) {
+          // Wait a rising edge on the clock
+          dut.clockDomain.waitRisingEdge()
+      }
+      dut.clockDomain.waitRisingEdge(8)
       dut.clockDomain.waitRisingEdge(8)
     }
   }
