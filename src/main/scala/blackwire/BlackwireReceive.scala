24a3df901e0132999d2f090364943f75cb971ac5
Leon Woestenberg <leon@sidebranch.com>
2022-12-01 17:00:07 +0100

    Forward drop flag from UpSizer to AxisToCorundum.

--- src/main/scala/blackwire/BlackwireReceive.scala
+++ src/main/scala/blackwire/BlackwireReceive.scala
@@ -13,14 +13,18 @@ import corundum._
 // companion object
 object BlackwireReceive {
   def main(args: Array[String]) {
-    SpinalVerilog(new BlackwireReceive())
-    //SpinalVhdl(new BlackwireReceive())
-    
-    val report = SpinalConfig(
-      mode = VHDL
-    ).generate(new BlackwireReceive())
-
-    report.mergeRTLSource("merge")
+    def spinalConfig = SpinalConfig(
+      targetDirectory = "build/rtl",
+      mode = VHDL,
+      // synchronous resets
+      defaultConfigForClockDomains = ClockDomainConfig(
+        resetKind = SYNC
+      ),
+      // change (un)signed I/Os at toplevel into std_logic_vector
+      onlyStdLogicVectorAtTopLevelIo = true
+    )
+    val vhdlReport = spinalConfig.generateVhdl(new BlackwireReceive())
+    //vhdlReport.mergeRTLSource("merge")
   }
 }
 
@@ -118,7 +122,6 @@ case class BlackwireReceive() extends Component {
     when (p.isFirst) {
       //s_length.assignFromBits(s.payload.fragment.asBits()(16, 16 bits))
       s_length.assignFromBits(p.payload.fragment(16, 16 bits).resize(12))
-      // @TODO put tag_valid in length field
     }
     s <-< p
     // @NOTE drop is valid on last beat
@@ -138,7 +141,7 @@ case class BlackwireReceive() extends Component {
   upsizer.io.sink_drop := s_drop
   u << upsizer.io.source
   val u_length = upsizer.io.source_length
-  val u_drop = False //upsizer.io.source_drop
+  val u_drop = upsizer.io.source_drop
 
   printf("Upsizer Latency = %d clock cycles.\n", LatencyAnalysis(s.valid, u.valid))
 
@@ -251,6 +254,13 @@ object BlackwireReceiveSim {
       var tkeep0 = BigInt(0)
       var pause = false
 
+      dut.io.sink.valid #= valid0
+      dut.io.sink.payload.tdata #= 0
+      dut.io.sink.last #= last0
+      dut.io.sink.payload.tkeep #= tkeep0
+      dut.io.sink.payload.tuser #= 0
+
+
       dut.clockDomain.waitSampling()
 
       // 000  4c 61 64 69 65 73 20 61 6e 64 20 47 65 6e 74 6c  Ladies and Gentl
@@ -268,59 +278,89 @@ object BlackwireReceiveSim {
 // "CCCCLLLLb315SSSS", DDDD=port 5555 (0x15b3)
 // "00000000FFFF0000"
 
-      val plaintext = Vector(
-        //      <-------- Ethernet header --------------> <-IPv4 header IHL=5 protocol=0x11->                         <--5555,5555,len0x172-> <----Wireguard Type 4 ------------------------> < L a  d  i  e  s
-        BigInt("01 02 03 04 05 06 01 02 03 04 05 06 01 02 45 11 22 33 44 55 66 77 88 11 00 00 00 00 00 00 00 00 00 00 15 b3 15 b3 01 72 00 00 04 00 00 00 11 22 33 44 c1 c2 c3 c4 c5 c6 c7 c8 4c 61 64 69 65 73".split(" ").reverse.mkString(""), 16),
-        //          a  n  d     G  e  n  t  l  e  m  e  n     o  f     t  h  e     c  l  a  s  s     o  f     '  9  9  :     I  f     I     c  o  u  l  d     o  f  f  e  r     y  o  u     o  n  l  y     o  n
-        BigInt("20 61 6e 64 20 47 65 6e 74 6c 65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73 73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63 6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f 6e 6c 79 20 6f 6e".split(" ").reverse.mkString(""), 16),
-        //       e     t  i  p     f  o  r     t  h  e     f  u  t  u  r  e  ,     s  u  n  s  c  r  e  e  n     w  o  u  l  d     b  e     i  t  . <---------- Poly 1305 Tag (16 bytes) --------->
-        BigInt("65 20 74 69 70 20 66 6f 72 20 74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73 63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69 74 2e 13 05 13 05 13 05 13 05 13 05 13 05 13 05 13 05 00 00 00 00".split(" ").reverse.mkString(""), 16)
-      )
-      // 64 - 6 = 58 bytes for all headers
-      // 3 * 64 bytes - 4 = 188 bytes for full Ethernet packet (as above)
-      // 188 - 58 = 130 bytes for encrypted/decrypted (16 bytes ceiling padded) payload and the Poly1305 tag
-      // 130 - 16 = 114 bytes for encrypted/decrypted (16 bytes ceiling padded) )payload
-      // 114 bytes fits in 8 128-bit words
-      var packet_length = 16 + 114 // bytes
-      var remaining = packet_length
+      var remaining_packets = 3
+      val inter_packet_gap = 1
+      while (remaining_packets > 0) {
 
-      var word_index = 0
-      // iterate over frame content
-      while (remaining > 0) {
-        printf("remaining = %d\n", remaining)
-        val tkeep_len = if (remaining >= keepWidth) keepWidth else remaining;
-        printf("tkeep_len = %d\n", tkeep_len)
-        valid0 = (Random.nextInt(8) > 2)
-        valid0 &= !pause
-        if (pause) pause ^= (Random.nextInt(16) >= 15)
-        if (!pause) pause ^= (Random.nextInt(128) >= 127)
+        val plaintext1 = Vector(
+          //      <-------- Ethernet header --------------> <-IPv4 header IHL=5 protocol=0x11->                         <--5555,5555,len0x172-> <----Wireguard Type 4 ------------------------> < L a  d  i  e  s
+          BigInt("01 02 03 04 05 06 01 02 03 04 05 06 01 02 45 11 22 33 44 55 66 77 88 11 00 00 00 00 00 00 00 00 00 00 15 b3 15 b3 01 72 00 00 04 00 00 00 11 22 33 44 c1 c2 c3 c4 c5 c6 c7 c8 4c 61 64 69 65 73".split(" ").reverse.mkString(""), 16),
+          //          a  n  d     G  e  n  t  l  e  m  e  n     o  f     t  h  e     c  l  a  s  s     o  f     '  9  9  :     I  f     I     c  o  u  l  d     o  f  f  e  r     y  o  u     o  n  l  y     o  n
+          BigInt("20 61 6e 64 20 47 65 6e 74 6c 65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73 73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63 6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f 6e 6c 79 20 6f 6e".split(" ").reverse.mkString(""), 16),
+          //       e     t  i  p     f  o  r     t  h  e     f  u  t  u  r  e  ,     s  u  n  s  c  r  e  e  n     w  o  u  l  d     b  e     i  t  . <---------- Poly 1305 Tag (16 bytes) --------->
+          BigInt("65 20 74 69 70 20 66 6f 72 20 74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73 63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69 74 2e 13 05 13 05 13 05 13 05 13 05 13 05 13 05 13 05 00 00 00 00".split(" ").reverse.mkString(""), 16)
+        )
+        // 64 - 6 = 58 bytes for all headers
+        // 3 * 64 bytes - 4 = 188 bytes for full Ethernet packet (as above)
+        // 188 - 58 = 130 bytes for encrypted/decrypted (16 bytes ceiling padded) payload and the Poly1305 tag
+        // 130 - 16 = 114 bytes for encrypted/decrypted (16 bytes ceiling padded) )payload
+        // 114 bytes fits in 8 128-bit words
+        //var packet_length = 16 + 114 // bytes
 
-        assert(tkeep_len <= keepWidth)
-        tkeep0 = 0
-        data0 = 0
-        if (valid0) {
-          last0 = (remaining <= keepWidth)
-          for (i <- 0 until tkeep_len) {
-            tkeep0 = (tkeep0 << 1) | 1
+        val plaintext = Vector(
+          //      <-------- Ethernet header --------------> <-IPv4 header IHL=5 protocol=0x11->                         <--5555,5555,len0x172-> <----Wireguard Type 4 ------------------------>< L  a  d  i  e  s
+          BigInt("01 02 03 04 05 06 01 02 03 04 05 06 01 02 45 11 22 33 44 55 66 77 88 11 00 00 00 00 00 00 00 00 00 00 15 b3 15 b3 01 72 00 00 04 00 00 00 00 00 00 01 00 01 02 03 04 05 06 07 D3 1A 8D 34 64 8E".split(" ").reverse.mkString(""), 16),
+          //          a  n  d     G  e  n  t  l  e  m  e  n     o  f     t  h  e     c  l  a  s  s     o  f     '  9  9  :     I  f     I     c  o  u  l  d     o  f  f  e  r     y  o  u     o  n  l  y     o  n
+          BigInt("60 DB 7B 86 AF BC 53 EF 7E C2 A4 AD ED 51 29 6E 08 FE A9 E2 B5 A7 36 EE 62 D6 3D BE A4 5E 8C A9 67 12 82 FA FB 69 DA 92 72 8B 1A 71 DE 0A 9E 06 0B 29 05 D6 A5 B6 7E CD 3B 36 92 DD BD 7F 2D 77".split(" ").reverse.mkString(""), 16),
+          //       e     t  i  p     f  o  r     t  h  e     f  u  t  u  r  e  ,     s  u  n  s  c  r  e  e  n     w  o  u  l  d     b  e     i  t  . 
+          //                                                                                                                                                                                    <-Poly 1305 Tag...
+          BigInt("8B 8C 98 03 AE E3 28 09 1B 58 FA B3 24 E4 FA D6 75 94 55 85 80 8B 48 31 D7 BC 3F F4 DE F0 8E 4B 7A 9D E5 76 D2 65 86 CE C6 4B 61 16 0C 03 CE 35 6C D7 A6 76 63 FB DD EF FF 64 20 57 AD C1 AF 44".split(" ").reverse.mkString(""), 16),
+          //      ...Poly 1305 Tag (16 bytes)->
+          //BigInt("E7 01 50 87 55 CA 0A CC 8D 4E 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00".split(" ").reverse.mkString(""), 16)
+          BigInt("E7 01 50 87 55 CA 0A CC 8D 4E 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00".split(" ").reverse.mkString(""), 16)
+        )
+        var packet_length = 3 * 64 + 10 //bytes
+
+        var remaining = packet_length
+
+        var word_index = 0
+        // iterate over frame content
+        while (remaining > 0) {
+          printf("remaining = %d\n", remaining)
+          val tkeep_len = if (remaining >= keepWidth) keepWidth else remaining;
+          printf("tkeep_len = %d\n", tkeep_len)
+          valid0 = (Random.nextInt(8) > 2)
+          valid0 &= !pause
+          if (pause) pause ^= (Random.nextInt(16) >= 15)
+          if (!pause) pause ^= (Random.nextInt(128) >= 127)
+
+          assert(tkeep_len <= keepWidth)
+          tkeep0 = 0
+          data0 = 0
+          if (valid0) {
+            last0 = (remaining <= keepWidth)
+            for (i <- 0 until tkeep_len) {
+              tkeep0 = (tkeep0 << 1) | 1
+            }
+          }
+
+          dut.io.sink.valid #= valid0
+          dut.io.sink.payload.tdata #= plaintext(word_index)
+          dut.io.sink.last #= last0
+          dut.io.sink.last #= last0
+          dut.io.sink.payload.tkeep #= tkeep0
+          dut.io.sink.payload.tuser #= 0
+
+          dut.io.source.ready #= (Random.nextInt(8) > 1)
+
+          // Wait a rising edge on the clock
+          dut.clockDomain.waitRisingEdge()
+
+          if (dut.io.sink.ready.toBoolean & dut.io.sink.valid.toBoolean) {
+            remaining -= tkeep_len
+            word_index += 1
           }
         }
+        dut.io.sink.valid #= false
 
-        dut.io.sink.valid #= valid0
-        dut.io.sink.payload.tdata #= plaintext(word_index)
-        dut.io.sink.last #= last0
-        dut.io.sink.payload.tkeep #= tkeep0
+        printf("remaining = %d after while (remaining > 0))\n", remaining)
+        assert(remaining == 0)
 
-        dut.io.source.ready #= (Random.nextInt(8) > 1)
+        dut.clockDomain.waitRisingEdge(inter_packet_gap)
 
-        // Wait a rising edge on the clock
-        dut.clockDomain.waitRisingEdge()
+        remaining_packets -= 1
+      } // while remaining_packets
 
-        if (dut.io.sink.ready.toBoolean & dut.io.sink.valid.toBoolean) {
-          remaining -= tkeep_len
-          word_index += 1
-        }
-      }
-      dut.io.sink.valid #= false
       dut.io.source.ready #= true
 
       dut.clockDomain.waitRisingEdge(8 + 32 + 290)
