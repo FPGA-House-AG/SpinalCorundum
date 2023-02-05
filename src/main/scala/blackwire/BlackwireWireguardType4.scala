ba6771244b2c7498cd30935322cf8389f20ae8a0
Leon Woestenberg <leon@sidebranch.com>
2023-02-05 01:22:12 +0100

    BlackwireWireguardType4 tested against C reference.
    Signed-off-by: Leon Woestenberg <leon@sidebranch.com>

--- src/main/scala/blackwire/BlackwireWireguardType4.scala
+++ src/main/scala/blackwire/BlackwireWireguardType4.scala
@@ -108,35 +108,35 @@ object BlackwireWireguardType4Sim {
     val keepWidth = dataWidth/8
     SimConfig
     .withGhdl
-    .withVcdWave
-    .addRtl(s"MaximVHDL/imports/project_1/ChaCha20.vhd")
-    .addRtl(s"MaximVHDL/new/AEAD_ChaCha_Poly.vhd")
+    .withFstWave
+    .addRtl(s"../ChaCha20Poly1305/src/ChaCha20.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/AEAD_ChaCha_Poly.vhd")
 
-    .addRtl(s"MaximVHDL/imports/project_1/q_round.vhd")
-    .addRtl(s"MaximVHDL/imports/project_1/diag_round.vhd")
-    .addRtl(s"MaximVHDL/imports/project_1/col_round.vhd")
-    .addRtl(s"MaximVHDL/imports/project_1/half_round.vhd")
-    .addRtl(s"MaximVHDL/new/test_top_ChaCha.vhd")
-    .addRtl(s"MaximVHDL/new/Poly1305.vhd")
-    .addRtl(s"MaximVHDL/new/ChaCha20_128.vhd")
-    .addRtl(s"MaximVHDL/new/mul136_mod_red.vhd")
-    .addRtl(s"MaximVHDL/new/mul_red_pipeline.vhd")
-    .addRtl(s"MaximVHDL/new/test_top_mod_red.vhd")
-    .addRtl(s"MaximVHDL/new/ChaCha_int.vhd")
-    .addRtl(s"MaximVHDL/new/r_power_n.vhd")
-    .addRtl(s"MaximVHDL/mul_gen_0.vhd")
-    .addRtl(s"MaximVHDL/new/mul_36.vhd")
-    .addRtl(s"MaximVHDL/new/mul_72.vhd")
-    .addRtl(s"MaximVHDL/new/mul_144.vhd")
-    .addRtl(s"MaximVHDL/new/mod_red_1305.vhd")
-    .addRtl(s"MaximVHDL/new/Poly_1305_pipe_top.vhd")
-    //.addRtl(s"MaximVHDL/new/test_top_Poly.vhd")
-    .addRtl(s"MaximVHDL/new/Poly_1305_pipe.vhd")
-    .addRtl(s"MaximVHDL/new/AEAD_decryption_top.vhd")
-    .addRtl(s"MaximVHDL/new/AEAD_top.vhd")
-    .addRtl(s"MaximVHDL/new/Poly_pipe_top_test.vhd")
-    .addRtl(s"MaximVHDL/new/AEAD_decryption.vhd")
-    .addRtl(s"MaximVHDL/AEAD_decryption_wrapper.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/q_round.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/diag_round.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/col_round.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/half_round.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/test_top_ChaCha.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/Poly1305.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/ChaCha20_128.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/mul136_mod_red.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/mul_red_pipeline.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/test_top_mod_red.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/ChaCha_int.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/r_power_n.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/mul_gen_0.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/mul_36.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/mul_72.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/mul_144.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/mod_red_1305.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/Poly_1305_pipe_top.vhd")
+    //.addRtl(s"../ChaCha20Poly1305/src/test_top_Poly.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/Poly_1305_pipe.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/AEAD_decryption_top.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/AEAD_top.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/Poly_pipe_top_test.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/AEAD_decryption.vhd")
+    .addRtl(s"../ChaCha20Poly1305/src/AEAD_decryption_wrapper.vhd")
 
     //.withFstWave
     .doSim(BlackwireWireguardType4()){dut =>
@@ -155,40 +155,37 @@ object BlackwireWireguardType4Sim {
 
       dut.clockDomain.waitSampling()
 
-      // 000  4c 61 64 69 65 73 20 61 6e 64 20 47 65 6e 74 6c  Ladies and Gentl
-      // 016  65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73  emen of the clas
-      // 032  73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63  s of '99: If I c
-      // 048  6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f  ould offer you o
-      // 064  6e 6c 79 20 6f 6e 65 20 74 69 70 20 66 6f 72 20  nly one tip for
-      // 080  74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73  the future, suns
-      // 096  63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69  creen would be i
-      // 112  74 2e                                            t.
-
-      // @TODO add Poly1305 tag
-      //val plaintext = Vector(
-      //  BigInt("04 00 00 00 11 22 33 44 c1 c2 c3 c4 c5 c6 c7 c8 4c 61 64 69 65 73 20 61 6e 64 20 47 65 6e 74 6c 65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73 73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63".split(" ").reverse.mkString(""), 16),
-      //  BigInt("6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f 6e 6c 79 20 6f 6e 65 20 74 69 70 20 66 6f 72 20 74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73 63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69".split(" ").reverse.mkString(""), 16),
-      //  BigInt("74 2e 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00".split(" ").reverse.mkString(""), 16)
-      //)
-
-      //// reversed byte order of TKEEP
-      //val plaintext = Vector(
-      //  BigInt("04 00 00 00 00 00 00 01 00 01 02 03 04 05 06 07 4c 61 64 69 65 73 20 61 6e 64 20 47 65 6e 74 6c 65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73 73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63".split(" ")/*.reverse*/.mkString(""), 16),
-      //  BigInt("6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f 6e 6c 79 20 6f 6e 65 20 74 69 70 20 66 6f 72 20 74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73 63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69".split(" ")/*.reverse*/.mkString(""), 16),
-      //  BigInt("74 2e 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00".split(" ")/*.reverse*/.mkString(""), 16)
-      //)
           dut.io.sink.tuser #= 0
 
       var remaining_packets = 2
       val inter_packet_gap = 1
       while (remaining_packets > 0) {
-        // reversed byte order of TKEEP
-        val plaintext = Vector(
-          BigInt("8B 72 92 DA 69 FB FA 82 12 67 A9 8C 5E A4 BE 3D D6 62 EE 36 A7 B5 E2 A9 FE 08 6E 29 51 ED AD A4 C2 7E EF 53 BC AF 86 7B DB 60 8E 64 34 8D 1A D3 07 06 05 04 03 02 01 00 01 00 00 00 90 00 00 04".split(" ")/*.reverse*/.mkString(""), 16),
+        
+        val plaintext3 = Vector(
+          // nonce hardcoded 07 00 00 00 40 41 42 43 44 45 46 47 in the aead_decryption_wrapper.vhd
+          BigInt("8B 72 92 DA 69 FB FA 82 12 67 A9 8C 5E A4 BE 3D D6 62 EE 36 A7 B5 E2 A9 FE 08 6E 29 51 ED AD A4 C2 7E EF 53 BC AF 86 7B DB 60 8E 64 34 8D 1A D3 47 46 45 44 43 42 41 40 01 00 00 00 70 00 00 04".split(" ")/*.reverse*/.mkString(""), 16),
           BigInt("4B C6 CE 86 65 D2 76 E5 9D 7A 4B 8E F0 DE F4 3F BC D7 31 48 8B 80 85 55 94 75 D6 FA E4 24 B3 FA 58 1B 09 28 E3 AE 03 98 8C 8B 77 2D 7F BD DD 92 36 3B CD 7E B6 A5 D6 05 29 0B 06 9E 0A DE 71 1A".split(" ")/*.reverse*/.mkString(""), 16),
-          BigInt("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 4E 8D CC 0A CA 55 87 50 01 E7 44 AF C1 AD 57 20 64 FF EF DD FB 63 76 A6 D7 6C 35 CE 03 0C 16 61".split(" ")/*.reverse*/.mkString(""), 16)
+          BigInt("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 5a 12 89 4a 77 9a bf 9f ab 31 03 df 54 17 1e 80 64 FF EF DD FB 63 76 A6 D7 6C 35 CE 03 0C 16 61".split(" ")/*.reverse*/.mkString(""), 16)
         )
-
+        // nonce hardcoded 00 00 00 00 (40 41 42 43 44 45 46 47) in the aead_decryption_wrapper.vhd
+        //       nonce         <= x"00000000"&msg_reordered(63 downto 0);
+        val plaintext = Vector(
+          BigInt("04 00 00 80 00 00 00 01 40 41 42 43 44 45 46 47 a4 79 cb 54 62 89 46 d6 f4 04 2a 8e 38 4e f4 bd 2f bc 73 30 b8 be 55 eb 2d 8d c1 8a aa 51 d6 6a 8e c1 f8 d3 61 9a 25 8d b0 ac 56 95 60 15 b7 b4".split(" ").reverse.mkString(""), 16),
+          BigInt("93 7e 9b 8e 6a a9 57 b3 dc 02 14 d8 03 d7 76 60 aa bc 91 30 92 97 1d a8 f2 07 17 1c e7 84 36 08 16 2e 2e 75 9d 8e fc 25 d8 d0 93 69 90 af 63 c8 20 ba 87 e8 a9 55 b5 c8 27 4e f7 d1 0f 6f af d0".split(" ").reverse.mkString(""), 16),
+          BigInt("46 47 1b 14 57 76 ac a2 f7 cf 6a 61 d2 16 64 25 2f b1 f5 ba d2 ee 98 e9 64 8b b1 7f 43 2d cc e4 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00".split(" ").reverse.mkString(""), 16)
+        )
+        // reversed byte order of TKEEP
+        val plaintext4 = Vector(
+          BigInt("8B 72 92 DA 69 FB FA 82 12 67 A9 8C 5E A4 BE 3D D6 62 EE 36 A7 B5 E2 A9 FE 08 6E 29 51 ED AD A4 C2 7E EF 53 BC AF 86 7B DB 60 8E 64 34 8D 1A D3 47 46 45 44 43 42 41 40 01 00 00 00 80 00 00 04".split(" ")/*.reverse*/.mkString(""), 16),
+          BigInt("4B C6 CE 86 65 D2 76 E5 9D 7A 4B 8E F0 DE F4 3F BC D7 31 48 8B 80 85 55 94 75 D6 FA E4 24 B3 FA 58 1B 09 28 E3 AE 03 98 8C 8B 77 2D 7F BD DD 92 36 3B CD 7E B6 A5 D6 05 29 0B 06 9E 0A DE 71 1A".split(" ")/*.reverse*/.mkString(""), 16),
+          BigInt("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 e4 cc 2d 43 7f b1 8b 64 e9 98 ee d2 ba f5 b1 2f 64 FF EF DD FB 63 76 A6 D7 6C 35 CE 03 0C 16 61".split(" ")/*.reverse*/.mkString(""), 16)
+        )
+        // reversed byte order of TKEEP
+        val plaintext2 = Vector(
+          BigInt("8B 72 92 DA 69 FB FA 82 12 67 A9 8C 5E A4 BE 3D D6 62 EE 36 A7 B5 E2 A9 FE 08 6E 29 51 ED AD A4 C2 7E EF 53 BC AF 86 7B DB 60 8E 64 34 8D 1A D3 47 46 45 44 43 42 41 40 01 00 00 00 90 00 00 04".split(" ")/*.reverse*/.mkString(""), 16),
+          BigInt("4B C6 CE 86 65 D2 76 E5 9D 7A 4B 8E F0 DE F4 3F BC D7 31 48 8B 80 85 55 94 75 D6 FA E4 24 B3 FA 58 1B 09 28 E3 AE 03 98 8C 8B 77 2D 7F BD DD 92 36 3B CD 7E B6 A5 D6 05 29 0B 06 9E 0A DE 71 1A".split(" ")/*.reverse*/.mkString(""), 16),
+          BigInt("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 e4 cc 2d 43 7f b1 8b 64 e9 98 ee d2 ba f5 b1 2f 64 FF EF DD FB 63 76 A6 D7 6C 35 CE 03 0C 16 61".split(" ")/*.reverse*/.mkString(""), 16)
+        )
         // 512/8
         var packet_length = 64 + 64 + 16 + 16 // bytes
         var remaining = packet_length
@@ -291,25 +288,25 @@ case class BlackwireWireguardType4Bench() extends Component {
   source.last := False
   when (source.ready) {
     when (counter === 0) {
-    //source.tdata := B"512'h040000900000000100010203040506074c616469657320616e642047656e746c656d656e206f662074686520636c617373206f66202739393a20496620492063"
       source.tdata := B"512'h8B7292DA69FBFA821267A98C5EA4BE3D_D662EE36A7B5E2A9FE086E2951EDADA4_C27EEF53BCAF867BDB608E64348D1AD3_07060504030201000100000090000004";
-
       source.tkeep := B"64'hFFFFFFFFFFFFFFFF"
       source.valid := True
     }
     .elsewhen (counter === 1) {
-    //source.tdata := B"512'h6f756c64206f6666657220796f75206f6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73637265656e20776f756c642062652069"
       source.tdata := B"512'h4BC6CE8665D276E59D7A4B8EF0DEF43F_BCD731488B8085559475D6FAE424B3FA_581B0928E3AE03988C8B772D7FBDDD92_363BCD7EB6A5D605290B069E0ADE711A";
-
       source.tkeep := B"64'hFFFFFFFFFFFFFFFF"
-    }.elsewhen (counter === 2) {
-    //source.tdata := B"512'h742e000102030405060708090a0b0c0d0e0f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-      source.tdata := B"512'h00000000000000000000000000000000_00000000000000000000000000000000_4E8DCC0ACA55875001E744AFC1AD5720_64FFEFDDFB6376A6D76C35CE030C1661";
-
-    //source.tkeep := B"64'hFFFFc00000000000"
-      source.tkeep := B"64'h000000000000FFFF"
+    }
+    .elsewhen (counter === 2) {
+      source.tdata := B"512'h00000000000000000000000000000000_00000000000000000000000000000000_5a12894a779abf9fab3103df54171e80_64FFEFDDFB6376A6D76C35CE030C1661";
+      source.tkeep := B"64'h00000000FFFFFFFF"
       source.last  := True
     }
+    .elsewhen (counter === 3) {
+      source.valid := False
+      source.last  := False
+    }
+
+
     counter := counter + 1
   }
 
