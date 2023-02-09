d07b891d555bfc3b43b7c6f48135b15f7590d469
Leon Woestenberg <leon@sidebranch.com>
2023-02-09 15:32:33 +0100

    Match renamed IO signals in AEAD_decryption_wrapper.

--- src/main/scala/blackwire/ChaCha20Poly1305EncryptBlackbox.scala
+++ src/main/scala/blackwire/ChaCha20Poly1305EncryptBlackbox.scala
@@ -5,18 +5,18 @@ import corundum._
 import spinal.core._
 import spinal.lib._
 
-// Define ChaCha20Poly1305Decrypt / AEAD_decryption_wrapper
-// This is called AEAD_decryption_wrapper by Maxim, renamed here
-class AEAD_decryption_wrapper() extends BlackBox {
+// Define ChaCha20Poly1305Encrypt / AEAD_encryption_wrapper
+// This is called AEAD_encryption_wrapper by Maxim, renamed here
+class AEAD_encryption_wrapper() extends BlackBox {
   // Define IO of the VHDL entity / Verilog module
   val io = new Bundle {
     val clk = in Bool()
     val rst = in Bool()
 
-    val sink_tvalid  = in Bool()
-    val sink_tlast   = in Bool()
-    val sink_tdata   = in UInt(128 bits)
-    val sink_tready  = out Bool()
+    val sink_tvalid = in Bool()
+    val sink_tlast  = in Bool()
+    val sink_tdata  = in UInt(128 bits)
+    val sink_tready = out Bool()
 
     val in_key   = in UInt(256 bits)
 
@@ -35,16 +35,16 @@ class AEAD_decryption_wrapper() extends BlackBox {
 }
 
 // companion object
-object BlackwireChaCha20Poly1305DecryptSpinal {
+object BlackwireChaCha20Poly1305EncryptSpinal {
   //def main(args: Array[String]) {
-  //  SpinalVerilog(new ChaCha20Poly1305DecryptSpinal())
-  //  SpinalVhdl(new ChaCha20Poly1305DecryptSpinal())
+  //  SpinalVerilog(new ChaCha20Poly1305EncryptSpinal())
+  //  SpinalVhdl(new ChaCha20Poly1305EncryptSpinal())
   //}
 }
 
 
-// Define ChaCha20Poly1305Decrypt
-case class ChaCha20Poly1305DecryptSpinal() extends Component {
+// Define ChaCha20Poly1305Encrypt
+case class ChaCha20Poly1305EncryptSpinal() extends Component {
   // Define IO of the VHDL entity / Verilog module
   val io = new Bundle {
     val sink   = slave Stream(Fragment(Bits(128 bits)))
@@ -52,7 +52,7 @@ case class ChaCha20Poly1305DecryptSpinal() extends Component {
     val key    = in Bits (256 bit)
     val tag_valid = out Bool()
   }
-  val vhdl = new AEAD_decryption_wrapper()
+  val vhdl = new AEAD_encryption_wrapper()
 
   // decrypted output
   val d = Stream(Fragment(Bits(128 bits)))
@@ -65,8 +65,8 @@ case class ChaCha20Poly1305DecryptSpinal() extends Component {
   vhdl.io.sink_tdata  := U(io.sink.payload.fragment)
   vhdl.io.sink_tlast  := io.sink.payload.last
   // pass-through READY outside of the VHDL block, not READY after LAST
-  io.sink.ready             := d.ready & !after_last
-  vhdl.io.in_key  := U(io.key)
+  io.sink.ready       := d.ready & !after_last
+  vhdl.io.in_key      := U(io.key)
 
   d.valid                := vhdl.io.source_tvalid
   d.payload.fragment     := B(vhdl.io.source_tdata)
