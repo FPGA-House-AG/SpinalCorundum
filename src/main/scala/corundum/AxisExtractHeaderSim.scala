bf523978990055ee0fa5a880a8dd28520d56f007
Leon Woestenberg <leon@sidebranch.com>
2023-02-12 11:41:38 +0100

    AxisExtractHeaderSim enhance test using scoreboard.

--- src/main/scala/corundum/AxisExtractHeaderSim.scala
+++ src/main/scala/corundum/AxisExtractHeaderSim.scala
@@ -3,6 +3,7 @@ package corundum
 import spinal.core._
 import spinal.sim._
 import spinal.core.sim._
+import spinal.lib.sim.{ScoreboardInOrder, SimData}
 
 import scala.util.Random
 
@@ -11,9 +12,11 @@ object AxisExtractHeaderSim {
     val dataWidth = 32
     val maxDataValue = scala.math.pow(2, dataWidth).intValue - 1
     val keepWidth = dataWidth/8
+    val headerWidthBytes = 3 //Random.nextInt(dataWidth/8)
+
     SimConfig
     .withFstWave
-    .doSim(new AxisExtractHeader(dataWidth/*bits*/, 3/*bytes header*/)){dut =>
+    .doSim(new AxisExtractHeader(dataWidth/*bits*/, headerWidthBytes)){dut =>
 
       SimTimeout(100000 * 10)
 
@@ -25,13 +28,16 @@ object AxisExtractHeaderSim {
       //Fork a process to generate the reset and the clock on the dut
       dut.clockDomain.forkStimulus(period = 10)
 
-      var modelState = 0
-
       //StreamFragmentGenerator(event: x, packetData: y, dataType: CorundumFrame)
 
-      //Create a new thread
-      val myNewThread = fork {
+      val dataScoreboard = ScoreboardInOrder[BigInt]()
+
+      // monitor output (source) of DUT
+      val monitorThread = fork {
+        // number of nibbles for printf formatters
         val dw = dataWidth / 4
+        val hw = headerWidthBytes * 2
+        // keep packet reception state
         var not_last_seen = false
         var in_packet_continuation = false
         var first_beat = false
@@ -39,25 +45,35 @@ object AxisExtractHeaderSim {
         var remaining = 0
         while (true) {
           dut.clockDomain.waitSamplingWhere(dut.io.source.valid.toBoolean & dut.io.source.ready.toBoolean)
+          // keep track of packet boundaries and remaining bytes
           first_beat = !in_packet_continuation
           if (first_beat) {
             packet_length = dut.io.source_length.toInt
-            remaining = dut.io.source_length.toInt
+            remaining = packet_length
+
+            // store DUT output bytes from header into scoreboard
+            var ref0 = dut.io.header.toBigInt
+            printf(s"HEADER == 0x%0${hw}X %02d bytes\n", ref0, headerWidthBytes)
+            for (i <- 0 until headerWidthBytes) {
+              dataScoreboard.pushDut(ref0 & 0xff)
+              ref0 = ref0 >> 8
+            }
           }
-          //var clean_data = dut.io.source.payload.fragment.toBigInt
-          //if (remaining < dataWidth / 8) {
-          //  var mask = BigInt(0)
-          //  for (byte <- 0 until remaining) {
-          //    mask <<= 8
-          //    mask = (mask | 0xFF)
-          //  }
-          //  clean_data = mask
-          //}
-          
-          printf(s"DATA == 0x%0${dw}X %02d/%02d %s%s\n", dut.io.source.payload.fragment.toBigInt,
-            remaining, packet_length,
+
+          val bytes_in_beat = if (remaining >= dataWidth/8) dataWidth/8 else remaining
+
+          printf(s"DATA == 0x%0${dw}X %02d/%02d %02d bytes %s%s\n",
+            dut.io.source.payload.fragment.toBigInt,
+            remaining, packet_length, bytes_in_beat,
             if (first_beat) "*" else s" ",
             if (dut.io.source.payload.last.toBoolean) "L" else s" ")
+          // store DUT output bytes from payload into scoreboard
+          var data1 = dut.io.source.payload.fragment.toBigInt
+          for (i <- 0 until bytes_in_beat) {
+              dataScoreboard.pushDut(data1 & 0xff)
+              data1 = data1 >> 8
+          }
+          // keep track of packet boundaries and remaining bytes
           in_packet_continuation = !dut.io.source.payload.last.toBoolean
           remaining = if (remaining >= dataWidth/8) remaining - dataWidth/8 else 0
         }
@@ -75,6 +91,7 @@ object AxisExtractHeaderSim {
       // iterate over all frames to generate
       for (packet_idx <- 1 until 31) {
         var packet_length = packet_idx //1 + Random.nextInt(if (packet_idx > 3400) keepWidth else maxPacketSizeBytes)
+        if (packet_idx == 7) packet_length = dataWidth/8
         //val packet_length = packet_idx match {
         //case (packet_idx > 3400): 1 + Random.nextInt(keepWidth)
         //case (packet_idx > 4800): 0
@@ -133,6 +150,14 @@ object AxisExtractHeaderSim {
           if (dut.io.sink.ready.toBoolean & dut.io.sink.valid.toBoolean) {
             remaining -= tkeep_len
             byte_counter += tkeep_len
+            // only packets more than header length will pass
+            if (packet_length > headerWidthBytes) {
+              // keep track of bytes going into the DUT
+              for (i <- 0 until tkeep_len) {
+                  dataScoreboard.pushRef(data0 & 0xff)
+                  data0 = data0 >> 8
+              }
+            }
           }
         }
         // after each packet, introduce delay for now
