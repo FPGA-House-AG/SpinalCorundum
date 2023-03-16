6ae4e27103dafa0cdf92af2fe0c4f872d5af0736
Leon Woestenberg <leon@sidebranch.com>
2023-03-16 17:00:30 +0100

    Use optimized encrypter from SpinalHDL.

--- src/main/scala/blackwire/ChaCha20Poly1305EncryptBlackbox.scala
+++ src/main/scala/blackwire/ChaCha20Poly1305EncryptBlackbox.scala
@@ -7,7 +7,7 @@ import spinal.lib._
 
 // Define ChaCha20Poly1305Encrypt / AEAD_encryption_wrapper
 // This is called AEAD_encryption_wrapper by Maxim, renamed here
-class AEAD_encryption_wrapper() extends BlackBox {
+class AEAD_encryption_wrapper_kar() extends BlackBox {
   // Define IO of the VHDL entity / Verilog module
   val io = new Bundle {
     val clk = in Bool()
@@ -25,7 +25,7 @@ class AEAD_encryption_wrapper() extends BlackBox {
     val source_tdata   = out UInt(128 bits)
     val source_tready  = in Bool()
 
-    val tag_valid       = out Bool()
+    val header_out     = out UInt(128 bits)
   }
 
   // Map the current clock domain to the io.clk and io.rst pins
@@ -36,10 +36,10 @@ class AEAD_encryption_wrapper() extends BlackBox {
 
 // companion object
 object BlackwireChaCha20Poly1305EncryptSpinal {
-  //def main(args: Array[String]) {
-  //  SpinalVerilog(new ChaCha20Poly1305EncryptSpinal())
-  //  SpinalVhdl(new ChaCha20Poly1305EncryptSpinal())
-  //}
+  def main(args: Array[String]) {
+    SpinalVerilog(new ChaCha20Poly1305EncryptSpinal())
+    SpinalVhdl(new ChaCha20Poly1305EncryptSpinal())
+  }
 }
 
 // Define ChaCha20Poly1305Encrypt
@@ -48,13 +48,13 @@ case class ChaCha20Poly1305EncryptSpinal() extends Component {
   val io = new Bundle {
     val sink   = slave Stream(Fragment(Bits(128 bits)))
     val source = master Stream(Fragment(Bits(128 bits)))
-    val key    = in Bits (256 bit)
-    val tag_valid = out Bool()
+    val key    = in Bits(256 bit)
+    val header_out = out Bits(128 bits)
   }
-  val vhdl = new AEAD_encryption_wrapper()
+  val vhdl = new AEAD_encryption_wrapper_kar()
 
-  // decrypted output
-  val d = Stream(Fragment(Bits(128 bits)))
+  // encrypted output
+  val e = Stream(Fragment(Bits(128 bits)))
 
   // enforce one idle cycle after last beat, this is
   // required by VHDL ChaCha20Poly1305
@@ -64,13 +64,17 @@ case class ChaCha20Poly1305EncryptSpinal() extends Component {
   vhdl.io.sink_tdata  := U(io.sink.payload.fragment)
   vhdl.io.sink_tlast  := io.sink.payload.last
   // pass-through READY outside of the VHDL block, not READY after LAST
-  io.sink.ready       := d.ready// & !after_last
+  io.sink.ready       := e.ready// & !after_last
   vhdl.io.in_key      := U(io.key)
 
-  d.valid                := vhdl.io.source_tvalid
-  d.payload.fragment     := B(vhdl.io.source_tdata)
-  d.payload.last         := vhdl.io.source_tlast
-  vhdl.io.source_tready  := d.ready
+  e.valid                := vhdl.io.source_tvalid
+  e.payload.fragment     := B(vhdl.io.source_tdata)
+  e.payload.last         := vhdl.io.source_tlast
+  vhdl.io.source_tready  := e.ready
+
+  io.header_out := B(vhdl.io.header_out)
+
+  io.source << e
 
   // Execute the function renameAxiIO after the creation of the component
   addPrePopTask(() => CorundumFrame.renameAxiIO(io))
