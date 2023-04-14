7cc9dce73d570f65041f822e4415c32798374486
ddzigal <dzemil.dzigal@gmail.com>
2023-04-14 14:37:48 +0200

    Addition of basic structure of PreventReplay.scala Source consists of new ReceiveWindow class used for memory interaction. Component currently doesn't support any functionality. Incremental commits will introduce functionality, gradually. Change is needed for the RFC6479 implementation of PreventReplay.scala

--- src/main/scala/corundum/PreventReplay.scala
+++ src/main/scala/corundum/PreventReplay.scala
@@ -15,53 +15,38 @@ object PreventReplay {
   // generate VHDL and Verilog
   def main(args: Array[String]) {
     val verilogReport = Config.spinal.generateVerilog({
-      val toplevel = new PreventReplay(10, 32, 64)
+      val toplevel = new PreventReplay(32, 32, 64)
       toplevel
     })
     val vhdlReport = Config.spinal.generateVhdl({
-      val toplevel = new PreventReplay(10, 32, 64)
+      val toplevel = new PreventReplay(32, 32, 64)
       toplevel
     })
   }
 }
 
-case class PreventReplay(windowSize: Int,
-                         sessionIdWidth: Int,
-                         counterWidth: Int) extends Component {
+case class PreventReplay(windowSize:        Int,
+                         sessionIdWidth:    Int,
+                         counterWidth:      Int           
+                         ) extends Component {
 
   
+  class ReceiveWindow(windowSize : Int, sessionIdWidth : Int) extends Bundle {
+    var wt          = UInt(sessionIdWidth bits)
+    var bitmap      = Bits(windowSize bits)
+  }
+
   val io = new Bundle {
       val drop      = out Bool()
       val sessionId = in  UInt(sessionIdWidth bits)
       val counter   = in  UInt(counterWidth   bits)
   }
-
-  var result = RegInit(False)
-  var memory = Mem(Bits(counterWidth * windowSize bits), 1024)
-  memory.addAttribute("ram_style", "block") 
-
-
-  var data = memory.readSync(address = io.sessionId.resize(10 bits), 
-                             enable  = True)
   
-  for (j <- 0 until windowSize) {
+  var state  = new ReceiveWindow(windowSize, sessionIdWidth)
+  var result = RegInit(False)
+  var memory = Mem(Bits(windowSize bits), 1024)
 
-    when(data(j * counterWidth, counterWidth bits) === io.counter.asBits) {
-      
-      //var shiftedData = data(0, j * counterWidth bits) ## io.counter ## data.take((windowSize - j - 1) * counterWidth)//data(j * counterWidth, (counterWidth - j - 1) * windowSize bits) 
-      result := True
-    }.elsewhen(Bool(j == windowSize - 1) && data(j * counterWidth, counterWidth bits) =/= io.counter.asBits) {
-
-      var shiftedData = io.counter ## data(0, counterWidth * windowSize - counterWidth bits)
-      memory.write(address = io.sessionId.resize(10 bits), 
-                   data    = shiftedData,
-                   enable  = True)
-
-      result := False
-    }
-  }
-
-  io.drop := result
+  io.drop := False
 
 }
 
@@ -80,21 +65,6 @@ object PreventReplaySim {
       // Fork a process to generate the reset and the clock on the dut
       dut.clockDomain.forkStimulus(period = 10)
 
-      dut.clockDomain.waitRisingEdge()
-      dut.io.sessionId      #= 0
-      dut.io.counter        #= 0
-      dut.clockDomain.waitRisingEdge()
-      
-      dut.clockDomain.waitRisingEdge(10)
-      //Wait for some cycles
-      for(i <-0 until 20){
-        dut.io.sessionId    #= 1
-        dut.io.counter      #= i
-        //Wait for some cycles
-        dut.clockDomain.waitRisingEdge(5)
-      }
-      
-      
 
     }
   }
