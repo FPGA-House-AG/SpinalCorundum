3c76373596f89c280bb56c3bc9acefadbfc019a9
ddzigal <dzemil.dzigal@gmail.com>
2023-04-18 14:06:21 +0200

    Change of PreventReplay.scala
    Removed the width method from PreventReplay. Because it's not needed.
    We call getWidth after flattening
    state by calling asBits for this purpose.
    Added read_data as an output and implemented 2-cycle latency memory.
    This is to ensure we can pipeline our component.
    Added a ReadBundle read for this purpose, too.
    Changes in reference to commit cea4d242294a0f6b5d07c605c39a53f542ca1b31

--- src/main/scala/corundum/PreventReplay.scala
+++ src/main/scala/corundum/PreventReplay.scala
@@ -15,11 +15,11 @@ object PreventReplay {
   // generate VHDL and Verilog
   def main(args: Array[String]) {
     val verilogReport = Config.spinal.generateVerilog({
-      val toplevel = new PreventReplay(32, 32, 64, 1024)
+      val toplevel = new PreventReplay(64, 10, 64, 1024)
       toplevel
     })
     val vhdlReport = Config.spinal.generateVhdl({
-      val toplevel = new PreventReplay(32, 32, 64, 1024)
+      val toplevel = new PreventReplay(64, 10, 64, 1024)
       toplevel
     })
   }
@@ -32,30 +32,30 @@ case class PreventReplay(windowSize:        Int,
                          ) extends Component {
 
   // This class is our per-session state. 
-  case class ReceiveWindow(windowSize : Int, sessionIdWidth : Int) extends Bundle {
-    var wt          = UInt(sessionIdWidth bits)
-    var bitmap      = Bits(windowSize bits)
-
-    def width: BitCount = {
-      var totalWidth = 0
-      for(field <- this.elements){
-        totalWidth = totalWidth + field._2.getBitsWidth
-      }
-      totalWidth bits
-    }
+  case class ReceiveWindow(windowSize : Int, counterWidth : Int) extends Bundle {
+    var wt          = UInt(counterWidth bits)
+    var bitmap      = Bits(windowSize bits) 
   }
 
   val io = new Bundle {
       val drop      = out Bool()
       val sessionId = in  UInt(sessionIdWidth bits)
       val counter   = in  UInt(counterWidth   bits)
+      val read_data = out Bits(windowSize     bits)
+      val read      = ReadBundle(log2Up(numberOfSessions), counterWidth)
   }
   
-  var state  = ReceiveWindow(windowSize, sessionIdWidth)
+  var state  = ReceiveWindow(windowSize, counterWidth)
   var result = RegInit(False)
-  var memory = Mem(Bits(state.width), numberOfSessions)
+  var memory = Mem(Bits(state.asBits.getWidth bits), numberOfSessions)
 
-  io.drop := False
+
+  io.read_data := RegNext(memory.readSync(
+    enable  = io.read.enable,
+    address = io.read.addr
+  )(counterWidth-1 downto 0))
+    
+  io.drop      := False
 
 }
 
@@ -70,11 +70,14 @@ import scala.util.Random
 
 object PreventReplaySim {
   def main(args: Array[String]) {
-    SimConfig.withFstWave.doSim(new PreventReplay(10, 32, 64, 1024)){dut =>
+    SimConfig.withFstWave.doSim(new PreventReplay(64, 10, 64, 1024)){dut =>
       // Fork a process to generate the reset and the clock on the dut
       dut.clockDomain.forkStimulus(period = 10)
 
-
+      dut.clockDomain.waitRisingEdge(20)
+      dut.io.sessionId.assignBigInt(1)
+      dut.io.counter.assignBigInt(1)
+      dut.clockDomain.waitRisingEdge(20)
     }
   }
 }
