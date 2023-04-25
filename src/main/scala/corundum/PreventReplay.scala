9b5f135029a10003fb04ac05982d809150f9aed3
ddzigal <dzemil.dzigal@gmail.com>
2023-04-25 14:43:25 +0200

    Addition of GTKW config and change of PreventReplay.scala
    Finished first implementation, fully, of all branching for functionality.
    Testing finished in the case of sliding window, start of operations
    and if packet is too old.
    
    Added config for GTKW for this test to streamline testing.
    Further bugfixes started, changes needed because no functionality
    was present prior to this commit.
    
    Changes in reference to commit:
    d50a912db2497d9a796145384435141063f63e97

--- src/main/scala/corundum/PreventReplay.scala
+++ src/main/scala/corundum/PreventReplay.scala
@@ -15,11 +15,11 @@ object PreventReplay {
   // generate VHDL and Verilog
   def main(args: Array[String]) {
     val verilogReport = Config.spinal.generateVerilog({
-      val toplevel = new PreventReplay(32, 10, 64, 1024)
+      val toplevel = new PreventReplay(10, 10, 64, 1024)
       toplevel
     })
     val vhdlReport = Config.spinal.generateVhdl({
-      val toplevel = new PreventReplay(32, 10, 64, 1024)
+      val toplevel = new PreventReplay(10, 10, 64, 1024)
       toplevel
     })
   }
@@ -51,57 +51,58 @@ case class PreventReplay(windowSize:        Int,
   memory.initBigInt(Seq.fill(numberOfSessions)(0))
   memory.addAttribute("ram_style", "block") 
   
-  var data = memory.readSync(address = io.sessionId.resize(10 bits), 
+  var data = memory.readSync(address = io.sessionId.resize(log2Up(numberOfSessions) bits), 
                              enable  = True)
   
   state.wt     := data(windowSize, counterWidth bits).asUInt
   state.bitmap := data(0, windowSize bits)
 
-  var wt     = io.counter
-  var wb     = wt-windowSize+1
+  var s_val     = io.counter
+  var wb     = s_val-windowSize+1
 
-  var wt_ptr = wt % windowSize
+  var wt_ptr = s_val % windowSize
   var wb_ptr = wb % windowSize
 
-  when(wt === U(0, counterWidth bits)){
+
+
+  when(s_val === U(0, counterWidth bits)){
     // Start of operation or wrapped
     io.drop := False
-  }.elsewhen(wt > state.wt){
+  }.elsewhen(s_val > state.wt){
     // New packet, slide the window
-    var diff = wt - state.wt - 1
+    var diff = s_val - state.wt - 1
 
     for(i <- 0 until windowSize){
-      when((i > state.wt+1) & (i < wt)){
+      when((i > state.wt+1) & (i < s_val)){
         state.bitmap(i % windowSize)     := False
       }.elsewhen(i === state.wt+1){
         state.bitmap(i % windowSize)     := True
       }
     }
     
-    data = wt.asBits ## state.bitmap
-    memory(io.sessionId.resize(10 bits)) := data
+    data = s_val.asBits ## state.bitmap
+    memory(io.sessionId.resize(log2Up(numberOfSessions) bits)) := data
 
     io.drop := False
 
-  }.elsewhen(wt + U(windowSize, counterWidth bits) < state.wt){
+  }.elsewhen(s_val + U(windowSize, counterWidth bits) <= state.wt){
     // Too old packet
     io.drop := True
   }.otherwise{
     
     val conditionalReg = Reg(Bool())
-    conditionalReg := state.bitmap(wt_ptr.resize(5 bits))
+    conditionalReg := state.bitmap(wt_ptr.resize(log2Up(windowSize) bits))
     // S inside window, check the memory
     when(conditionalReg === False){
-      state.bitmap(wt_ptr.resize(5 bits)) := True 
-      data = wt.asBits ## state.bitmap
-      memory(io.sessionId.resize(10 bits)) := data
+      state.bitmap(wt_ptr.resize(log2Up(windowSize) bits)) := True 
+      data = s_val.asBits ## state.bitmap
+      memory(io.sessionId.resize(log2Up(numberOfSessions) bits)) := data
       io.drop := False
       //We haven't seen this packet yet. We set the bit in memory, and don't update the window
     }.otherwise{
       //We've seen this packet already, we drop it, and we don't update the window.
       io.drop := True
     }
-    
     
   }
 
@@ -122,16 +123,62 @@ import scala.util.Random
 
 object PreventReplaySim {
   def main(args: Array[String]) {
-    SimConfig.withFstWave.doSim(new PreventReplay(32, 10, 64, 1024)){dut =>
+    SimConfig.withFstWave.doSim(new PreventReplay(10, 10, 64, 1024)){dut =>
       // Fork a process to generate the reset and the clock on the dut
       dut.clockDomain.forkStimulus(period = 10)
 
       for(i <-0 until 20){
         dut.io.sessionId.assignBigInt(1)
         dut.io.counter.assignBigInt(i)
-        dut.clockDomain.waitRisingEdge()
+        dut.clockDomain.waitRisingEdge(2)
       }
 
+      for(i <-100 until 120){
+        dut.io.sessionId.assignBigInt(1)
+        dut.io.counter.assignBigInt(i)
+        dut.clockDomain.waitRisingEdge(2)
+      }
+
+      for(i <-100 until 110){
+        dut.io.sessionId.assignBigInt(1)
+        dut.io.counter.assignBigInt(i)
+        dut.clockDomain.waitRisingEdge(2)
+      }
+
+      //inside window
+      dut.io.sessionId.assignBigInt(1)
+      dut.io.counter.assignBigInt(157)
+      dut.clockDomain.waitRisingEdge(2)
+      //Window is now 157 - 150, but should not be dropped
+      for(i <-147 until 157){
+        dut.io.sessionId.assignBigInt(1)
+        dut.io.counter.assignBigInt(i)
+        dut.clockDomain.waitRisingEdge(2)
+      }
+      //Window is now 140 - 150, but should be dropped
+      for(i <-147 until 157){
+        dut.io.sessionId.assignBigInt(1)
+        dut.io.counter.assignBigInt(i)
+        dut.clockDomain.waitRisingEdge(2)
+      }
+
+      for(i <-147 until 157){
+        dut.io.sessionId.assignBigInt(1)
+        dut.io.counter.assignBigInt(i)
+        dut.clockDomain.waitRisingEdge(2)
+      }
+
+      for(i <-147 until 157){
+        dut.io.sessionId.assignBigInt(1)
+        dut.io.counter.assignBigInt(i)
+        dut.clockDomain.waitRisingEdge(2)
+      }
+
+      for(i <-147 until 157){
+        dut.io.sessionId.assignBigInt(1)
+        dut.io.counter.assignBigInt(i)
+        dut.clockDomain.waitRisingEdge(2)
+      }
     }
   }
 }
