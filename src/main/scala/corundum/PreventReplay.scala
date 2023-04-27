f71eeeb4dac6a6722b15066a4043c7e6719996de
ddzigal <dzemil.dzigal@gmail.com>
2023-04-27 17:44:04 +0200

    Reduced component cycle intervals to 2. Added new GTKW file for GTKW configuration for signal viewing purposes. PreventReplay.scala now passes basic test if we consider that one data cycle is 2 CLK cycles. This needs to be reduced to 1 CLK cycles:
    Further research and development of Hazard Detection section of the component
    Test this until the basic test succeeds.
    Expand test further and validate component.
    
    Code in reference to commit:
    1cca71efbda492338178d16a64a281121e039bb6

--- src/main/scala/corundum/PreventReplay.scala
+++ src/main/scala/corundum/PreventReplay.scala
@@ -15,11 +15,11 @@ object PreventReplay {
   // generate VHDL and Verilog
   def main(args: Array[String]) {
     val verilogReport = Config.spinal.generateVerilog({
-      val toplevel = new PreventReplay(10, 10, 64, 1024)
+      val toplevel = new PreventReplay(10, 10, 16, 1024)
       toplevel
     })
     val vhdlReport = Config.spinal.generateVhdl({
-      val toplevel = new PreventReplay(10, 10, 64, 1024)
+      val toplevel = new PreventReplay(10, 10, 16, 1024)
       toplevel
     })
   }
@@ -41,75 +41,147 @@ case class PreventReplay(windowSize:        Int,
       val drop      = out Bool()
       val sessionId = in  UInt(sessionIdWidth bits)
       val counter   = in  UInt(counterWidth   bits)
-      val read_data = out Bits(windowSize     bits)
-      val read      = ReadBundle(log2Up(numberOfSessions), counterWidth)
+      //val read_data = out Bits(windowSize     bits)
+      //val read      = ReadBundle(log2Up(numberOfSessions), counterWidth)
   }
   
   var state  = ReceiveWindow(windowSize, counterWidth)
-  var result = RegInit(UInt(2 bits))
+  var result = UInt(2 bits)
   result := 0
+  var hazard_case = UInt(2 bits)
+  hazard_case := 0
   var memory = Mem(Bits(state.asBits.getWidth bits), numberOfSessions)
   memory.initBigInt(Seq.fill(numberOfSessions)(0))
-  memory.addAttribute("ram_style", "block") 
-  
-  var data = memory.readSync(address = io.sessionId.resize(log2Up(numberOfSessions) bits), 
-                             enable  = True)
-  
-  state.wt     := data(windowSize, counterWidth bits).asUInt
-  state.bitmap := data(0, windowSize bits)
 
-  var s_val     = io.counter
+  //var addressReg = Reg(UInt(sessionIdWidth bits))
+  //addressReg    := RegNext(io.sessionId.resize(log2Up(numberOfSessions) bits))
+  /*mem read latency 2 cycles*/
+  /*FIRST STAGE DELAY*/
+  var d1_sessionId = RegNext(io.sessionId).init(0)
+  var d1_counter   = RegNext(io.counter).init(0)
+  /*SECOND STAGE DELAY*/
+  var d2_data      = RegNext(memory.readSync(address = d1_sessionId.resize(log2Up(numberOfSessions) bits), enable  = True))
+  var d2_sessionId = RegNext(d1_sessionId).init(0)
+  var d2_counter   = RegNext(d1_counter).init(0)
+  var d2_sop       = True
+  var d2_valid     = RegNext(True) 
+
+  /*THIRD STAGE DELAY*/ /*HERE WE DRIVE THE OUTPUT*/
+  var d3_data      = RegNext(d2_data).init(0)
+  var d3_sessionId = RegNext(d2_sessionId).init(0)
+  var d3_counter   = RegNext(d2_counter).init(0)
+  var d3_sop       = RegNext(d2_sop)
+  var d3_valid     = RegNext(d2_valid)
+
+  /*FOURTH STAGE DELAY*/ /*REGISTER THE STATE AND DATA ADDRESS FOR WRITING*/ 
+  var d4_sessionId = RegNext(d3_sessionId).init(0)
+  var d4_counter   = RegNext(d3_counter).init(0)
+  var d4_sop       = RegNext(d3_sop)
+  var d4_valid     = RegNext(d3_valid)
+  var d4_data      = RegNext(d3_data)//
+
+
+  /*FIFTH STAGE DELAY*/ /*REMEMBER WRITTEN DATA*/
+  var d5_data      = RegNext(d4_data)
+  var d5_sessionId = RegNext(d4_sessionId).init(0)
+  var d5_counter   = RegNext(d4_counter).init(0)
+  var d5_sop       = RegNext(d4_sop)
+  var d5_valid     = RegNext(d4_valid)
+  //io.drop         := d4_valid
+  var start_of_ops = d2_valid & d3_valid & d4_valid & d5_valid
+  memory.readWriteSync(d3_sessionId, data = d3_data, enable = True, write = True)
+  /*SOLVE RACE CONDITIONS*/
+  var take_from_d3 = False
+  var take_from_d4 = False
+  var take_from_d5 = False
+  
+  //var dataReg    = Reg(Bits(counterWidth+windowSize bits))
+  var counterReg = d2_counter
+  var sessionReg = d2_sessionId
+
+  
+  
+  when(d5_data(windowSize, counterWidth bits).asUInt > d2_data(windowSize, counterWidth bits).asUInt){
+    //dataReg    = d5_data
+    state.wt     := d5_data(windowSize, counterWidth bits).asUInt
+    state.bitmap := d5_data(0, windowSize bits)
+    hazard_case := 3
+  }.elsewhen(d4_data(windowSize, counterWidth bits).asUInt > d2_data(windowSize, counterWidth bits).asUInt){
+    state.wt     := d4_data(windowSize, counterWidth bits).asUInt
+    state.bitmap := d4_data(0, windowSize bits)
+    hazard_case := 2
+  }.elsewhen(d3_data(windowSize, counterWidth bits).asUInt > d2_data(windowSize, counterWidth bits).asUInt){
+    state.wt     := d3_data(windowSize, counterWidth bits).asUInt
+    state.bitmap := d3_data(0, windowSize bits)
+    hazard_case := 1
+  }.otherwise{
+    state.wt     := d2_data(windowSize, counterWidth bits).asUInt
+    state.bitmap := d2_data(0, windowSize bits)
+    hazard_case := 0
+  }
+  
+  //state.wt     := dataReg(windowSize, counterWidth bits).asUInt
+  //state.bitmap := dataReg(0, windowSize bits)
+
+  var s_val    = counterReg
   var s_ptr = s_val % windowSize
 
-
-  when(s_val === U(0, counterWidth bits)){
-    // Start of operation or wrapped
-    result := 0
-    io.drop := False
-  }.elsewhen(s_val > state.wt){
-    // New packet, slide the window
-    var diff = s_val - state.wt - 1
-    result := 1
-
-    for(i <- 0 until windowSize){
-      when((i > state.wt+1) & (i < s_val)){
-        state.bitmap(i % windowSize)     := False
-      }.elsewhen(i === state.wt+1){
-        state.bitmap(i % windowSize)     := True
-      }
-    }
-    
-    data = s_val.asBits ## state.bitmap
-    memory(io.sessionId.resize(log2Up(numberOfSessions) bits)) := data
-
-    io.drop := False
-
-  }.elsewhen(s_val + U(windowSize, counterWidth bits) <= state.wt){
-    // Too old packet
-    result := 2
-    io.drop := True
-  }.otherwise{
-    result := 3
-    val conditionalReg = Reg(Bool())
-    conditionalReg := state.bitmap(s_ptr.resize(log2Up(windowSize) bits))
-    // S inside window, check the memory
-    when(conditionalReg === False){
-      state.bitmap(s_ptr.resize(log2Up(windowSize) bits)) := True 
-      data = s_val.asBits ## state.bitmap
-      memory(io.sessionId.resize(log2Up(numberOfSessions) bits)) := data
+  when(start_of_ops === True){
+    when(s_val === U(0, counterWidth bits)){
+      // Start of operation or wrapped
+      result := 0
       io.drop := False
-      //We haven't seen this packet yet. We set the bit in memory, and don't update the window
-    }.otherwise{
-      //We've seen this packet already, we drop it, and we don't update the window.
+    }.elsewhen(s_val > state.wt){
+      // New packet, slide the window
+      var diff = s_val - state.wt - 1
+      result := 1
+
+      for(i <- 0 until windowSize){
+        when((i > state.wt+1) & (i < s_val)){
+          state.bitmap(i % windowSize)     := False
+        }.elsewhen(i === state.wt+1){
+          state.bitmap(i % windowSize)     := True
+        }
+      }
+
+      d3_data := s_val.asBits ## state.bitmap
+      d2_data := s_val.asBits ## state.bitmap
+      //memory(io.sessionId.resize(log2Up(numberOfSessions) bits)) := s_val.asBits ## state.bitmap //////////////////////////////////////////////
+
+      io.drop := False
+
+    }.elsewhen(s_val + U(windowSize, counterWidth bits) <= state.wt){
+      // Too old packet
+      result := 2
       io.drop := True
+    }.otherwise{
+      result := 3
+      var conditionalReg = Reg(Bool())
+      conditionalReg := state.bitmap(s_ptr.resize(log2Up(windowSize) bits)) === True// && d3_counter =/= d2_counter
+      // S inside window, check the memory
+      when(conditionalReg === False){
+        //memory(io.sessionId.resize(log2Up(numberOfSessions) bits)) := s_val.asBits ## state.bitmap ////////////////////////////////////////////
+        state.bitmap(s_ptr.resize(log2Up(windowSize) bits)) := True 
+        when(state.bitmap.andR){
+          io.drop := True
+        }.otherwise{
+          io.drop := False
+        } 
+        //We haven't seen this packet yet. We set the bit in memory, and don't update the window
+      }.otherwise{
+        //We've seen this packet already, we drop it, and we don't update the window.
+        io.drop := True
+        
+      }
+      //d4_data := state.wt ## state.bitmap
+      d3_data := state.wt ## state.bitmap
+      d2_data := state.wt ## state.bitmap
+      
     }
-    
+  }.otherwise{
+    io.drop := False
   }
 
-  io.read_data := RegNext(memory.readSync(
-    enable  = io.read.enable,
-    address = io.read.addr)
-  (counterWidth until counterWidth+windowSize))
 }
 
 
@@ -123,9 +195,11 @@ import scala.util.Random
 
 object PreventReplaySim {
   def main(args: Array[String]) {
-    SimConfig.withFstWave.doSim(new PreventReplay(10, 10, 64, 1024)){dut =>
+    SimConfig.withFstWave.doSim(new PreventReplay(10, 10, 16, 1024)){dut =>
       // Fork a process to generate the reset and the clock on the dut
       dut.clockDomain.forkStimulus(period = 10)
+      dut.io.sessionId.assignBigInt(0)
+      dut.io.counter.assignBigInt(0)
 
       for(i <-0 until 20){
         dut.io.sessionId.assignBigInt(1)
@@ -133,12 +207,12 @@ object PreventReplaySim {
         dut.clockDomain.waitRisingEdge()
       }
 
-      for(i <-100 until 120){
+      for(i <-0 until 20){
         dut.io.sessionId.assignBigInt(1)
         dut.io.counter.assignBigInt(i)
         dut.clockDomain.waitRisingEdge()
       }
-
+      /*
       for(i <-100 until 110){
         dut.io.sessionId.assignBigInt(1)
         dut.io.counter.assignBigInt(i)
@@ -178,7 +252,7 @@ object PreventReplaySim {
         dut.io.sessionId.assignBigInt(1)
         dut.io.counter.assignBigInt(i)
         dut.clockDomain.waitRisingEdge()
-      }
+      }*/
     }
   }
 }
