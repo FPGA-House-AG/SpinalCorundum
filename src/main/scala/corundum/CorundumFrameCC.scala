8df1f123c5e335c42bc871f9864b32f3cfb995be
Leon Woestenberg <leon@sidebranch.com>
2023-05-04 00:30:34 +0200

    CorundumFrameCC.scala: axis_async_fifo.v blackbox w/ benefit:
    The axis_async_fifo.tcl script will insert the correct timing constraints. Finger crossed, but this script is a piece of art.
    
    Signed-off-by: Leon Woestenberg <leon@sidebranch.com>

---
+++ src/main/scala/corundum/CorundumFrameCC.scala
@@ -0,0 +1,138 @@
+package corundum
+
+import spinal.lib.bus.misc._
+import spinal.lib.bus.amba4.axi._
+
+import spinal.core._
+import spinal.lib._
+
+import scala.math.pow
+import java.util.Base64
+
+class axis_async_fifo(depth: Int = 8, pushCD: ClockDomain, popCD: ClockDomain,
+  DATA_WIDTH : Int = 512, USER_WIDTH : Int = 1,
+  KEEP_ENABLE : Int = 1, LAST_ENABLE : Int = 1) extends BlackBox {
+  addGeneric("DATA_WIDTH", DATA_WIDTH)
+  addGeneric("DEPTH", depth)
+  addGeneric("KEEP_ENABLE", KEEP_ENABLE)
+  addGeneric("LAST_ENABLE", LAST_ENABLE)
+  addGeneric("USER_WIDTH", USER_WIDTH)
+  val KEEP_WIDTH = if (KEEP_ENABLE > 0) ((DATA_WIDTH+7)/8) else 0
+  val ID_WIDTH = 0
+  val DEST_WIDTH = 0
+  // Define IO of the VHDL entity / Verilog module
+  val io = new Bundle {
+    /*
+     * AXI input
+     */
+    val s_clk = in Bool()
+    val s_rst = in Bool()
+    val s_axis_tdata = in Bits(DATA_WIDTH bits)
+    val s_axis_tkeep = in Bits(KEEP_WIDTH bits)
+    val s_axis_tvalid = in Bool()
+    val s_axis_tready = out Bool()
+    val s_axis_tlast = in Bool()
+    val s_axis_tid = in Bits(ID_WIDTH bits)
+    val s_axis_tdest = in Bits(DEST_WIDTH bits)
+    val s_axis_tuser = in Bits(USER_WIDTH bits)
+
+    /*
+     * AXI output
+     */
+    val m_clk = in Bool()
+    val m_rst = in Bool()
+    val m_axis_tdata = out Bits(DATA_WIDTH bits)
+    val m_axis_tkeep = out Bits(KEEP_WIDTH bits)
+    val m_axis_tvalid = out Bool()
+    val m_axis_tready = in Bool()
+    val m_axis_tlast = out Bool()
+    val m_axis_tid = out Bits(ID_WIDTH bits)
+    val m_axis_tdest = out Bits(DEST_WIDTH bits)
+    val m_axis_tuser = out Bits(USER_WIDTH bits)
+
+    /*
+     * Status
+     */
+    val s_status_overflow = out Bool()
+    val s_status_bad_frame = out Bool()
+    val s_status_good_frame = out Bool()
+    val m_status_overflow = out Bool()
+    val m_status_bad_frame = out Bool()
+    val m_status_good_frame = out Bool()
+  }
+  mapClockDomain(pushCD, clock = io.s_clk, reset = io.s_rst)
+  mapClockDomain(popCD,  clock = io.m_clk, reset = io.m_rst)
+
+  noIoPrefix()
+}
+
+// companion object
+object CorundumFrameCC {
+  def main(args: Array[String]) : Unit = {
+    SpinalVerilog(new CorundumFrameCC(8, ClockDomain.current, ClockDomain.current))
+    SpinalVhdl(new CorundumFrameCC(8, ClockDomain.current, ClockDomain.current))
+  }
+}
+case class CorundumFrameCC(val depth: Int,
+                           val pushClock: ClockDomain,
+                           val popClock: ClockDomain,
+                           val dataWidth: Int = 512
+                           ) extends Component {
+  // Define IO of the VHDL entity / Verilog module
+  val io = new Bundle {
+    val push =    slave Stream Fragment(CorundumFrame(dataWidth))
+    val pop = master Stream Fragment(CorundumFrame(dataWidth))
+  }
+  val verilog = new axis_async_fifo(depth, pushClock, popClock, dataWidth)
+
+  verilog.io.s_axis_tdata  := io.push.fragment.tdata
+  verilog.io.s_axis_tkeep  := io.push.fragment.tkeep
+  verilog.io.s_axis_tuser  := io.push.fragment.tuser
+  verilog.io.s_axis_tvalid := io.push.valid
+  verilog.io.s_axis_tlast  := io.push.last
+  io.push.ready             := verilog.io.s_axis_tready
+
+  io.pop.fragment.tdata := verilog.io.m_axis_tdata
+  io.pop.fragment.tkeep := verilog.io.m_axis_tkeep
+  io.pop.fragment.tuser := verilog.io.m_axis_tuser
+  io.pop.valid          := verilog.io.m_axis_tvalid
+  io.pop.last           := verilog.io.m_axis_tlast
+  verilog.io.m_axis_tready := io.pop.ready
+
+  // Execute the function renameAxiIO after the creation of the component
+  addPrePopTask(() => CorundumFrame.renameAxiIO(io))
+}
+
+// companion object
+object KeyStreamCC {
+  def main(args: Array[String]) : Unit = {
+    SpinalVerilog(new KeyStreamCC(8, ClockDomain.current, ClockDomain.current))
+    SpinalVhdl(new KeyStreamCC(8, ClockDomain.current, ClockDomain.current))
+  }
+}
+case class KeyStreamCC(val depth: Int, val pushClock: ClockDomain,
+                               val popClock: ClockDomain,
+                               val dataWidth: Int = 256
+                               ) extends Component {
+  // Define IO of the VHDL entity / Verilog module
+  val io = new Bundle {
+    val push =    slave Stream Bits(dataWidth bits)
+    val pop = master Stream Bits(dataWidth bits)
+  }
+  val verilog = new axis_async_fifo(depth, pushClock, popClock,
+    DATA_WIDTH = dataWidth, USER_WIDTH = 0, KEEP_ENABLE = 0, LAST_ENABLE = 0)
+
+  verilog.io.s_axis_tdata  := io.push.payload
+  verilog.io.s_axis_tvalid := io.push.valid
+  verilog.io.s_axis_tlast  := True
+  verilog.io.s_axis_tkeep  := 0
+  verilog.io.s_axis_tuser  := 0
+  io.push.ready            := verilog.io.s_axis_tready
+
+  io.pop.payload        := verilog.io.m_axis_tdata
+  io.pop.valid          := verilog.io.m_axis_tvalid
+  verilog.io.m_axis_tready := io.pop.ready
+
+  // Execute the function renameAxiIO after the creation of the component
+  addPrePopTask(() => CorundumFrame.renameAxiIO(io))
+}
